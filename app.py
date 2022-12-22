dad48a2213489984b27f9bc0287f90fc7f157e75
zs <2084035767@qq.com>
2022-12-22 22:21:03 +0800

    历史上的今天

---
+++ app.py
@@ -0,0 +1,25 @@
+from datetime import date
+from flask import Flask, render_template
+import requests
+app = Flask(__name__)
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36 Edg/106.0.1370.47'
+}
+url = 'https://api.ahfi.cn/api/lsjt'
+params = {
+    'format': 'json'
+}
+with requests.get(url, headers=headers, params=params) as r:
+    day = r.json()['day']
+    content = r.json()['content']
+
+today = date.today()
+@app.route('/')
+def hello():  # put application's code here
+    return render_template("index.html",content=content,today=today)
+
+
+
+if __name__ == '__main__':
+    app.run()
