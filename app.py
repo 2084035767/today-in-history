06aafbb1562fe3c647084d9357a96181043438d0
zs <2084035767@qq.com>
2022-12-22 23:04:23 +0800

    更新

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from datetime import date
 from flask import Flask, render_template
 import requests
+
 app = Flask(__name__)
 
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36 Edg/106.0.1370.47'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 '
+                  'Safari/537.36 Edg/106.0.1370.47 '
 }
 url = 'https://api.ahfi.cn/api/lsjt'
 params = {
@@ -15,10 +17,11 @@ with requests.get(url, headers=headers, params=params) as r:
     content = r.json()['content']
 
 today = date.today()
-@app.route('/')
-def hello():  # put application's code here
-    return render_template("index.html",content=content,today=today)
 
+
+@app.route('/')
+def index():
+    return render_template("index.html", content=content, today=today)
 
 
 if __name__ == '__main__':
